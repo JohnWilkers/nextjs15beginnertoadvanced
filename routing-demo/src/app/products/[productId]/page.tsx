f90f0e3a4b49add1aaf1a4e0e147f76dc86c7d07
John <johnwilker2@gmail.com>
2025-08-28 16:40:02 -0300

    feat: Routing Metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,28 @@
+import { Metadata } from "next";
+
+type MetadataProps = {
+  params: Promise<{ productId: string}>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: MetadataProps): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`);
+    }, 100)
+  })
+  return {
+    title: `Product ${title}`,
+  }
+}
+
 export default async function ProductDetails({
   params,
-}: {
-  params: Promise<{productId: string}>;
-}) {
+}: 
+  MetadataProps
+) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
