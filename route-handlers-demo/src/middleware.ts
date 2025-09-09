81cd9849f469efc79bb5630fa155946565441d42
John <johnwilker2@gmail.com>
2025-09-09 08:57:31 -0300

    feat: Redirection Handling, Caching in Route Handlers, Middleware

---
+++ route-handlers-demo/src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // return NextResponse.redirect(new URL("/", request.url));
+
+  if (request.nextUrl.pathname === "/profile") {
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme");
+    if (!themePreference) {
+      response.cookies.set("theme", "dark");
+    }
+
+    response.headers.set("custom-header", "custom-value");
+
+    return response;
+
+    // return NextResponse.redirect(new URL("/hello, request."));
+
+    // if (request.nextUrl.pathname === "/profile") {
+    //   return NextResponse.redirect(new URL("/hello", request.nextUrl));
+    // }
+    // if (request.nextUrl.pathname === "/profile") {
+    //   return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+    // }
+  }
+}
+
+// export const config = {
+//   matcher: "/profile",
+// };
