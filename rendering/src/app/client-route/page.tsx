0afda75526f6a36edbf76a3600009d6c57081ada
John <johnwilker2@gmail.com>
2025-09-11 10:31:47 -0300

    feat: Generate Static Params, Dynamic Params, Streaming, Server Composition Patterns: Server Only Code, Third-Party Packages, Context Providers

---
+++ rendering/src/app/client-route/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useTheme } from '@/components/theme-provider';
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function ClientRoutePage() {
+  const theme = useTheme();
+  const settings = {
+    dots: true,
+  };
+  return (
+    <h1 style={{ color: theme.colors.primary }}>Client Route Page</h1>
+  );
+}
---
+++ rendering/src/app/server-route/page.tsx
@@ -0,0 +1,12 @@
+import { ImageSlider } from "@/components/ImageSlider";
+import { serverSideFunction } from "@/utils/server-utils";
+
+export default function ServerRoutePage() {
+  const result = serverSideFunction();
+  return (
+    <>
+      Server Route {result}
+      <ImageSlider />
+    </>
+  )
+}
