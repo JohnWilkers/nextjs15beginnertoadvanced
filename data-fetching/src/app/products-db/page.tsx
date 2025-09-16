30ce2e615afc355f037793da9c9e849f7c491f0b
John <johnwilker2@gmail.com>
2025-09-16 08:54:58 -0300

    feat: Updating Server Actions, useOptimistic Hook

--- data-fetching/src/app/products-db/page.tsx
+++ data-fetching/src/app/products-db/page.tsx
@@ -1,5 +1,5 @@
 import { getProducts } from "@/prisma-db";
-import Link from "next/link";
+import { ProductsDetail } from "./product-detail";
 
 export type Product = {
   id: number;
@@ -10,21 +10,6 @@ export type Product = {
 
 export default async function ProductsDBPage() {
   const products: Product[] = await getProducts();
-
-  return (
-    <ul className="space-y-4 p-4">
-      {products.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-xl font-semibold">
-            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-          </h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-        </li>
-      ))}
-    </ul>
-  );
+  
+  return <ProductsDetail products={products} />;
 };
